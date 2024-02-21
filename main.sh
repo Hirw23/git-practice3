37ff8ea1b9658c572b85c2090a93b26573e0330e
emmy <nshimiyee311@gmail.com>
2024-02-21 18:16:37 +0200

    added break lines for better visual separation

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 
 FILE="students-list_1023.txt"
 
+# Function to register a student
 function create_student {
     echo -n "Enter student ID: "
     read student_id
@@ -12,6 +13,8 @@ function create_student {
     echo "$student_id,$student_email,$student_age" >> $FILE
     echo "Student record created."
 }
+
+# Function to view a list of all students on the terminal
 function view_students {
     if [[ -f $FILE ]]; then
         echo "Listing all students:"
@@ -20,12 +23,16 @@ function view_students {
         echo "No student records found."
     fi
 }
+
+# Function to delete a students using their id
 function delete_student {
     echo -n "Enter student ID to delete: "
     read student_id
     grep -v "^$student_id," $FILE > temp_file && mv temp_file $FILE
     echo "Student record deleted."
 }
+
+# Function to update a students details using their id too
 function update_student {
     echo -n "Enter student ID to update: "
     read student_id
@@ -38,24 +45,33 @@ function update_student {
     mv temp_file $FILE
     echo "Student record updated."
 }
+
+# Function to exit from the program
 function exit_application {
-        echo "Exiting application."
+        echo "Exiting the program......"
         exit 0
     }
+
+# Main menu
 while true; do
-    echo " ===== ALU registration system ====:"
-    echo "1. Register a student record"
+    echo ""
+    echo "-----------------------------------------------------"
+    echo " ===== ALU REGISTRATION SYSTEM FOR SE NEW COHORT ===="
+    echo "-----------------------------------------------------"
+    echo ""
+    echo "1. Register a student into ALU system"
     echo "2. List of SE cohort of students"
     echo "3. Delete a student"
-    echo "4. Update a student record"
+    echo "4. Update a student"
     echo "5. Exit"
+    echo ""
     read operation
     case $operation in
-        1) create_student ;;
+    1) create_student ;;
 	2) view_students ;;
 	3) delete_student ;;
 	4) update_student ;;
 	5) exit_application ;;
-        *) echo "Invalid option. Please try again." ;;
+    *) echo "Invalid option. Please try again." ;;
     esac
 done
