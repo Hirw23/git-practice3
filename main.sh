738e886104cbe79d8b65b1c7aef6cd366e9b82b9
MairoPedroIsaac <mairopedroisaac@gmail.com>
2024-02-21 05:36:58 -0800

    added exit program function

--- main.sh
+++ main.sh
@@ -20,15 +20,20 @@ function view_students {
         echo "No student records found."
     fi
 }
-
+function exit_application {
+        echo "Exiting application."
+        exit 0
+    }
 while true; do
     echo " ===== ALU registration system ====:"
     echo "1. Register a student record"
     echo "2. List of SE cohort of students"
+    echo "5. Exit"
     read operation
     case $operation in
         1) create_student ;;
 	2) view_students ;;
+	5) exit_application ;;
         *) echo "Invalid option. Please try again." ;;
     esac
 done
